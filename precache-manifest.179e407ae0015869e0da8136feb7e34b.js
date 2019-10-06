b04e146a229d73f7d8655302f79508428b1af22a
alextana <alex.tana1992@gmail.com>
2019-10-06 22:12:56 +0100

    push 1

---
+++ precache-manifest.179e407ae0015869e0da8136feb7e34b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d213356b03ea6e766f0d460f7bb083a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80cec78969438f55fec9",
+    "url": "/static/css/main.6f15719d.chunk.css"
+  },
+  {
+    "revision": "1ae51faed45f0527cece",
+    "url": "/static/js/2.a12883f7.chunk.js"
+  },
+  {
+    "revision": "80cec78969438f55fec9",
+    "url": "/static/js/main.72db8d90.chunk.js"
+  },
+  {
+    "revision": "c7582b63b4638f1fa5a4",
+    "url": "/static/js/runtime-main.749a8642.js"
+  }
+]);
